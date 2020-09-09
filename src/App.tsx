e1144b8392115d4edd0f49120ccaa0a333c6f63c
alexandr96 <alex.pugachev96@gmail.com>
2020-09-09 18:25:49 +0300

    inter commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,46 @@
 import React from 'react';
 import Routes from './routes/Routes';
+import { configureStore } from '@reduxjs/toolkit';
+import rootReducer from './reducers/index';
+import { mainTheme } from './theme/main';
+import firebase from 'firebase/app';
+import { Provider as ReduxProvider } from 'react-redux';
+import { ThemeProvider as StyledCompProvider } from 'styled-components';
+import { FirebaseAuthProvider } from '@react-firebase/auth';
+
+const store = configureStore({
+  reducer: rootReducer
+});
+
+type Config = {
+  apiKey: string;
+  authDomain: string;
+  databaseURL: string;
+  projectId: string;
+  storageBucket: string;
+  messagingSenderId: string;
+  appId: string;
+}
+
+const config: Config = {
+  apiKey: process.env.REACT_APP_GOOGLE_API_ID ?? '',
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN ?? '',
+  databaseURL: process.env.REACT_APP_FIREBASE_DB_URL ?? '',
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID ?? '',
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET ?? '',
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID ?? '',
+  appId: process.env.REACT_APP_FIREBASE_APP_ID ?? ''
+};
 
 const App: React.FC = () => {
   return (
-    <Routes/>
+    <FirebaseAuthProvider firebase={firebase} {...config}>
+      <ReduxProvider store={store}>
+        <StyledCompProvider theme={mainTheme}>
+          <Routes/>
+        </StyledCompProvider>
+      </ReduxProvider>
+    </FirebaseAuthProvider>
   );
 };
 
--- src/index.tsx
+++ src/index.tsx
@@ -3,23 +3,10 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
-import { Provider } from 'react-redux';
-import { configureStore } from '@reduxjs/toolkit';
-import rootReducer from './reducers/index';
-import { ThemeProvider } from 'styled-components';
-import { mainTheme } from './theme/main';
-
-const store = configureStore({
-  reducer: rootReducer
-});
 
 ReactDOM.render(
   <React.StrictMode>
-    <Provider store={store}>
-      <ThemeProvider theme={mainTheme}>
-        <App/>
-      </ThemeProvider>
-    </Provider>
+    <App/>
   </React.StrictMode>,
   document.getElementById('app-root')
 );
